d1ba3839456051d6e4351be0951d2bbf59fc7991
Gavin King <gavin.king@acando.no>
2019-05-11 13:19:20 +0200

    Added another example with immer internal to a store

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import App from './App.svelte';
-import { writable } from 'svelte/store';
+import { things } from './store';
+import { get } from 'svelte/store';
 import produce from 'immer';
 
 const app = new App({
@@ -13,26 +14,23 @@ export default app;
 
 // /////////////////////////////////////////////////////////////////////////////
 // /////////////////////////////////////////////////////////////////////////////
-// /////////////////////////////////////////////////////////////////////////////
-
-// Create a simple svelte store with a single object as the first element
-export const things = writable([
-	{
-		foo: 'bar'
-	}
-]);
+// ////////////////////////////////////////////////////////////////////////////
 
 // We need to take a reference of the store contents on each fire of subscribe
 // so we can check it's equality.
 let prevValuesZero;
 
-// So the subscribe becomes the test
+// So the `subscribe` becomes the place to test if the store value is changed
 things.subscribe(values => {
+	let message = 'IMMUTABLE';
+
 	console.log('`things` subscribe fired');
 
 	if (prevValuesZero && values[0] === prevValuesZero) {
-		console.log('NOT IMMUTABLE', values[0].nameNotImportant);
+		message = `NOT ${message}`; // NOT IMMUTABLE
 	}
+
+	console.log(message, values[0].nameNotImportant);
 
 	prevValuesZero = values[0];
 });
@@ -40,7 +38,16 @@ things.subscribe(values => {
 // Just using the svelte update, the store contents is not immutable
 setTimeout(() => {
 	things.update(values => {
+		// mutate store data
 		values[0].nameNotImportant = 'svelte update';
+
+		// immediately check the store value again
+		const existingStoreValues = get(things);
+
+		if (existingStoreValues[0].nameNotImportant === 'svelte update') {
+			// this will happen
+			console.warn('store mutated in an update before `set` called');
+		}
 
 		return values;
 	});
@@ -52,3 +59,11 @@ setTimeout(() => {
 		draft[0].nameNotImportant = 'immer update';
 	}));
 }, 1000);
+
+// If we add the immer functionality inside our stores, developers do not have
+// to think about this when updating.
+setTimeout(() => {
+	things.immerUpdate(draft => {
+		draft[0].nameNotImportant = 'immer inside store update';
+	});
+}, 1500);
---
+++ src/store.js
@@ -0,0 +1,22 @@
+import { writable } from 'svelte/store';
+import produce from 'immer';
+
+const initialState = [
+	{
+		foo: 'bar'
+	}
+];
+
+function createThings() {
+  const { subscribe, update } = writable(initialState);
+
+  return {
+    update, // expose the original svelte update for test purposes
+    subscribe,
+
+    // expose an immer based update
+    immerUpdate: draftUpdater => update(produce(draftUpdater))
+  };
+}
+
+export const things = createThings();
