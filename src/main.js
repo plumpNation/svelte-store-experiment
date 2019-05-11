f14ec36d9b2dcb594279d7047ebb10d51899930e
Gavin King <gavin.king@acando.no>
2019-05-11 15:50:06 +0200

    fuck tabs, long live spaces

--- src/main.js
+++ src/main.js
@@ -4,10 +4,10 @@ import { get } from 'svelte/store';
 import produce from 'immer';
 
 const app = new App({
-	target: document.body,
-	props: {
-		name: 'Jeff'
-	}
+  target: document.body,
+  props: {
+    name: 'Jeff'
+  }
 });
 
 export default app;
@@ -22,57 +22,63 @@ let prevValues;
 
 // So the `subscribe` becomes the place to test if the store value is changed
 things.subscribe(values => {
-	console.log('`things` subscribe fired');
+  if (!prevValues) {
+    console.log('store initialised');
 
-	if (!prevValues) {
-		console.log('store initialised');
+  } else {
+    testMutation(prevValues, values);
 
-	} else {
-		testMutation(prevValues, values);
-	}
+    console.log('Test finished for', values[0].message);
+  }
 
-	prevValues = values;
+  prevValues = values;
 });
 
 // Just using the svelte update, the store contents is not immutable
 things.update(values => {
-	// mutate store data
-	values[0].message = 'svelte update';
-
-	// immediately check the store value again
+	// Just to illustrate that the reference update happens check the current store value:
 	const existingStoreValues = get(things);
 
 	console.assert(
-		existingStoreValues[0].message !== values[0].message,
-		'store mutated instantly within the update function by', values[0].message
-	);
+    existingStoreValues[0].message === values[0].message,
+    'values should be identical at this point', values[0].message
+  );
 
-	return values;
+  // mutate store data
+  values[0].message = 'svelte update';
+
+  // Ideally, values should be different at this point.
+  console.assert(
+    existingStoreValues[0].message !== values[0].message,
+    'store mutated instantly within the update function by', values[0].message
+  );
+
+  return values;
 });
 
 // By utilising immer, we can add that functionality in a very simple way
 things.update(produce(draft => {
-	draft[0].message = 'immer update';
+  draft[0].message = 'immer update';
 }));
 
 // If we add the immer functionality inside our stores, developers do not have
 // to think about this when updating.
 things.immerProduceUpdate(draft => {
-	draft[0].message = 'immer inside store update';
+  draft[0].message = 'immer inside store update';
 
-	// NOTE: we don't need to `return`, because internally it's `immer.produce`.
-	// This could be a source of confusion for devs, but it won't break anything
-	// if they do return the draft.
+  // NOTE: we don't need to `return`, because internally it's `immer.produce`.
+  // This could be a source of confusion for devs, but it won't break anything
+  // if they do return the draft.
 });
 
 function testMutation(prev, current) {
-	console.assert(
-		current !== prev,
-		'array has been mutated by', current[0].message
-	);
+  console.assert(
+    current !== prev,
+    'array has been mutated by', current[0].message
+  );
 
-	console.assert(
-		current[0] !== prev[0],
-		'array[0] has been mutated by', current[0].message
-	);
+  console.assert(
+    current[0] !== prev[0],
+    'array[0] has been mutated by', current[0].message
+  );
 }
--- src/store.js
+++ src/store.js
@@ -2,16 +2,20 @@ import { writable } from 'svelte/store';
 import produce from 'immer';
 
 const initialState = [
-	{
-		foo: 'bar'
-	}
+  {
+    foo: 'bar'
+  }
 ];
 
+/**
+ * By creating a custom store, we can expose our own update function and abstract
+ * the immutability away from the developer.
+ */
 function createThings() {
   const { subscribe, update } = writable(initialState);
 
   return {
-    update, // expose the original svelte update for test purposes
+    update, // expose the original svelte update (for test purposes)
     subscribe,
 
     // expose an immer based update
